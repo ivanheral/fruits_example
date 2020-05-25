a4a2d3dd78ab96bb03bf0ce9662111bedcbeaa77
ivanheral <ivanhernandezal@gmail.com>
2020-05-25 19:37:56 +0200

    fix default

--- src/foo.ts
+++ src/foo.ts
@@ -11,7 +11,7 @@ const randomFastFood = () =>
 const getRandomNumberBetween = (start, end) =>
   Math.floor(Math.random() * end) + start;
 
-export {
+export default {
   FRUITS,
   FAST_FOODS,
   randomFruit,
